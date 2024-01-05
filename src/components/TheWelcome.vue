08ae3ae37876bf56c7024d50423bf11b5bc52870
Nicolas Signori <n.signo@hotmail.com>
2024-01-05 17:28:40 -0500

    Work section

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -46,36 +46,176 @@ import WorkCard from "@/components/WorkCard.vue";
 
   const works = ref([
     {
+      title: "Développeur Web - Administrateur Système",
+      date: "Depuis janvier 2019 (4 ans)",
+      linkText: "Groupe Forman",
+      linkUrl: "https://groupeforman.ca/",
+      location: "Mont-Blanc",
+      icons : [
+        {
+          name: "EmberJS",
+          color: "orange"
+        },
+        {
+          name: "VueJs",
+          color: "green"
+        },
+        {
+          name: ".NET",
+          color: "purple"
+        },
+        {
+          name: "MySQL",
+          color: "teal"
+        },
+        {
+          name: "Linux"
+        },
+        {
+          name: "WordPress",
+          color: "blue"
+        },
+        {
+          name: "Azure",
+          color: "turquoise"
+        }
+      ],
+      moreInfo: {
+        list: [
+          "Création des sites web pour différents projets",
+          "Gestion des logiciels et des équipements avec Azure et SharePoint",
+          "Support informatique"
+        ],
+        extra: "<h3>Principaux Projets</h3>\n" +
+            "        <p>\n" +
+            "          <a href=\"https://skimontblanc.com/\">https://skimontblanc.com/</a><br>\n" +
+            "          <a href=\"https://groupeforman.ca/\">https://groupeforman.ca/</a><br>\n" +
+            "          <a href=\"https://ambiance-nature.com/\">https://ambiance-nature.com/</a><br>\n" +
+            "          <a href=\"https://latableedespionniers.com/\">https://latableedespionniers.com/</a><br>\n" +
+            "          Et bien plus!\n" +
+            "        </p>"
+      }
+    },
+    {
       title: "Développeur Frontend",
-      date: "Été 2022",
+      date: "Été 2022 - Stage (4 mois)",
       linkText: "Graitec inc.",
       linkUrl: "https://graitec.com/ca-en/",
-      location: "Montréal"
+      location: "Montréal",
+      moreInfo: {
+        list:[
+        "Migration d’une application Razor vers une technologie Web moderne",
+        "Création de différents modules dont la génération de rapport PDF, l’intégration de dessins techniques à l’interface et un système de commentaires",
+        "Organisation d’un atelier sur le développement Responsive avec HTML/CSS"
+      ]},
+      icons : [
+        {
+          name: "React",
+          color: "darkblue"
+        },
+        {
+          name: ".NET",
+          color: "purple"
+        },
+        {
+          name: "MySQL",
+          color: "teal"
+        }
+      ]
     },
     {
-      title: "Session d’études à l’étranger",
-      date: "Automne 2022",
-      linkText: "Université de Nantes – Polytech",
-      linkUrl: "https://www.univ-nantes.fr/",
-      logoUrl: "src/assets/logoNantes.png",
-      location: "France"
-    },
-    {
-      title: "Diplôme d’études collégiales",
-      date: "Informatique de gestion - 2018",
+      title: "Développeur Fullstack",
+      date: "Été 2022 - Stage (4 mois)",
+      linkText: "Bell Canada",
+      linkUrl: "https://www.bell.ca/",
+      location: "Montréal",
+      icons : [
+        {
+          name: "EmberJS",
+          color: "orange"
+        },
+        {
+          name: "VueJs",
+          color: "green"
+        },
+        {
+          name: ".NET",
+          color: "purple"
+        },
+        {
+          name: "SQL Server",
+          color: "darkred"
+        }
+      ],
+      moreInfo: {
+        list:[
+          "Support et ajout de fonctionnalités à une application servant à installer la fibre optique",
+          "Refonte du système de révisions et d’automatisation",
+          "Documentation des différentes fonctionnalités",
+          "Méthode de développement agile"
+      ]}
+    },{
+      title: "Développeur Fullstack",
+      date: "Été 2022 - Stage (4 mois)",
+      linkText: "Bell Canada",
+      linkUrl: "https://www.bell.ca/",
+      location: "Montréal",
+      icons : [
+        {
+          name: "EmberJS",
+          color: "orange"
+        },
+        {
+          name: ".NET",
+          color: "purple"
+        },
+        {
+          name: "SQL Server",
+          color: "darkred"
+        }
+      ],
+      moreInfo: {
+        list:[
+        "Mise en production d’une application servant à installer la fibre optique",
+        "Maintenance de plusieurs applications supportées par l’équipe"
+      ]}
+    },{
+      title: "Développeur Logiciel",
+      date: "Hiver 2018 - Stage Collégial (4 mois)",
+      linkText: "JBM Logic Inc.",
+      linkUrl: "https://www.jbmlogic.com/",
+      location: "Boucherville",
+      icons : [
+        {
+          name: "Apex",
+          color: "red"
+        },
+        {
+          name: "Oracle",
+          color: "red"
+        },
+        {
+          name: "PL/SQL",
+          color: "red"
+        }
+      ],
+      moreInfo: {
+        list:[
+          "Migration du logiciel développé par l’entreprise vers la technologie Apex 5.1",
+          "Création de composantes compatibles avec l’architecture actuelle"
+        ]}
+    },{
+      title: "Tuteur informatique",
+      date: "2015-2018 (3 ans)",
       linkText: "Collège de Bois-de-Boulogne",
       linkUrl: "https://www.bdeb.qc.ca/",
-      logoUrl: "src/assets/logoBdeb.svg",
-      location: "Montréal"
-    },
-    {
-      title: "Programme Explore",
-      date: "Immersion anglaise - 2017",
-      linkText: "University of Regina",
-      linkUrl: "https://www.uregina.ca/",
-      logoUrl: "src/assets/logoUofR.jpg",
-      location: "Saskatchewan"
+      location: "Montréal",
+      moreInfo: {
+        list:[
+          "Assister les étudiants à travers les cours d’informatique"
+        ]}
     }
+
   ])
 </script>
 
@@ -110,7 +250,9 @@ import WorkCard from "@/components/WorkCard.vue";
                    :linkText="work.linkText"
                    :linkUrl="work.linkUrl"
                    :logoUrl="work.logoUrl"
-                   :location="work.location" />
+                   :location="work.location"
+                   :icons="work.icons"
+                   :moreInfo="work.moreInfo"/>
     </div>
   </WelcomeItem>
 
@@ -164,7 +306,7 @@ import WorkCard from "@/components/WorkCard.vue";
  .cardList {
    display: grid;
    grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
-   align-items: stretch;
+   align-items: start;
    grid-gap: 0.5rem;
  }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,4 +3,12 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from "@fortawesome/free-solid-svg-icons"
+
+library.add(fas);
+
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
